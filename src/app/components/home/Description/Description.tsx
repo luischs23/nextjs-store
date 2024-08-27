f6cb87aa8bdea40bb69eaf26962498f4ff0e2beb
luischs23 <luisc.herreras@gmail.com>
2024-08-27 16:57:29 -0500

    Estilizado con Image de next

--- src/app/components/home/Description/Description.tsx
+++ src/app/components/home/Description/Description.tsx
@@ -2,15 +2,18 @@ import Image from 'next/image'
 
 export const Description = ()=>{
     return(
-        <section className='grid grid-cols-2 gap-x-9 max-w-xl mx-auto p-5 rounded-lg mb-24'>
+        <section className='grid grid-cols-2 gap-x-5 max-w-4xl mx-auto p-8 rounded-lg mb-10 items-center'>
+            <div className="flex items-center justify-center h-48 overflow-hidden">
             <Image 
                 src="/images/description.jpeg" 
                 alt="products marketplace" 
+                layout="responsive"
                 width={500}
-                height={500}
-                className="object-cover rounded-lg shadow-lg shadow-white/20 w-500 h-300"/>
-            <div>   
-                <h2 className="text-2xl font-bold text-secondary mb-0">Description</h2>
+                height={300}
+                className="object-cover rounded-lg shadow-lg shadow-white/20"/>
+            </div>
+            <div className="flex flex-col justify-center">   
+                <h2 className="text-2xl font-semibold text-secondary mb-4">Description</h2>
                 <p  className="text-lg leading-7 text-text-color">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis distinctio beatae, fugiat laudantium officia quas nostrum cupiditate, id aperiam expedita velit, eveniet modi similique minus. Deleniti aut architecto consequatur. Cumque!</p>
             </div>
         </section>
--- src/app/components/home/Hero/Hero.tsx
+++ src/app/components/home/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 export const Hero = ()=>{
     return(
         <section className="flex flex-col items-center">
-            <h1 className="text-8xl m-4 mb-4 font-bold linearg bg-clip-text text-transparent leading-normal">Hero</h1>
+            <h1 className="text-8xl m-4 mb-4 font-bold linearg bg-clip-text text-transparent leading-normal">Future World</h1>
             <h2 className="text-3xl font-semibold text-secondary">Empowering Your Tomorrow, Today!</h2>
         </section>
     )
