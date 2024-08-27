6b2de71d5402dc5f8e5ad140e36805fea03ccb2d
luischs23 <luisc.herreras@gmail.com>
2024-08-27 14:57:52 -0500

    Estilizado en description

--- src/app/components/home/Description/Description.tsx
+++ src/app/components/home/Description/Description.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
+
 export const Description = ()=>{
     return(
-        <section>
-            <h1>Descripción</h1>
+        <section className='grid grid-cols-2 gap-x-9 max-w-xl mx-auto p-5 rounded-lg mb-24'>
+            <Image 
+                src="/images/description.jpeg" 
+                alt="products marketplace" 
+                width={500}
+                height={500}
+                className="object-cover rounded-lg shadow-lg shadow-white/20 w-500 h-300"/>
+            <div>   
+                <h2 className="text-2xl font-bold text-secondary mb-0">Description</h2>
+                <p  className="text-lg leading-7 text-text-color">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis distinctio beatae, fugiat laudantium officia quas nostrum cupiditate, id aperiam expedita velit, eveniet modi similique minus. Deleniti aut architecto consequatur. Cumque!</p>
+            </div>
         </section>
     )
 }
