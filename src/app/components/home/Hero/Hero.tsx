13076b02bf9e0b4c4c8756af419f3c6bc729b9ec
luischs23 <luisc.herreras@gmail.com>
2024-08-27 09:54:28 -0500

    global.css

--- src/app/components/home/Hero/Hero.tsx
+++ src/app/components/home/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 export const Hero = ()=>{
     return(
-        <section>
-            <h1>Hero</h1>
+        <section className="m-32 bg-red-500">
+            <h1 className="bg-slate-400">Hero</h1>
         </section>
     )
 }
