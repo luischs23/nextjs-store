d5f2220d32025e6a2cd4ad709db88ab1a2139a12
luischs23 <luisc.herreras@gmail.com>
2024-08-28 10:39:04 -0500

    Obtener datos API shopify

--- src/app/components/home/MainProducts/MainProducts.tsx
+++ src/app/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,16 @@
-export const MainProducts = ()=>{
+const getProducts = async ()=>{
+    const response = await fetch(`https://${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-07/products.json`,{
+        headers: new Headers({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+        })
+    })
+    const data = await response.json()
+    return data
+}
+
+export const MainProducts = async()=>{
+    const products = await getProducts()
+    console.log(products)
     return(
         <section>
             <h1>MainProducts</h1>
