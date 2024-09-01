467c3e180f8889543ec2a387be6b612c16ae5857
luischs23 <luisc.herreras@gmail.com>
2024-08-31 20:00:29 -0500

    Product View

---
+++ src/components/product/ProductView/ProductView.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { ProductViewItemsOrder } from "./ProductViewItemsOrder";
+
+interface ProductViewProps {
+  product: ProductType
+}
+
+export const ProductView = ({ product }: ProductViewProps) => {
+
+  return (
+    <main className="max-w-[1200px] mx-auto grid grid-cols-[1fr_450px] gap-[5rem] mt-[5rem]">
+      <section className="justify-self-end">
+        <Image
+          loading="eager"
+          src={product.image}
+          width={500}
+          height={500}
+          quality={80}
+          alt={product.title}
+          className="rounded-lg"
+        />
+      </section>
+      <section className="flex flex-col">
+        <h1 className="text-2xl font-bold mt-0 mb-2">{product.title}</h1>
+        <p className="w-fit text-lg leading-6 tracking-tighter border-2 border-main-contrast text-text-color text-glow py-2 px-4 rounded-xl mt-4 mb-0">{product.tags}</p>
+        <p className="text-[1.125rem] leading-[1.5] tracking-[0.5px] my-4">
+          {product.description}
+        </p>
+        <span className="text-color-secondary text-[2.125rem] font-bold">
+          $ {product.price}
+        </span>
+        <ProductViewItemsOrder maxQuantity={product.quantity} />
+      </section>
+    </main>
+  )
+};
---
+++ src/components/product/ProductView/ProductViewItemsOrder.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { SyntheticEvent, useState } from "react";
+import { FaCartShopping } from 'react-icons/fa6';
+
+
+interface ProductViewItemsOrderProps {
+  maxQuantity: number,
+}
+
+export const ProductViewItemsOrder = ({ maxQuantity }: ProductViewItemsOrderProps) => {
+  const [counter, setCounter] = useState(1);
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+  };
+
+  const handleSubtract = (event: SyntheticEvent) => {
+    event.preventDefault();
+    if (counter === 1) return;
+    setCounter(counter - 1);
+  }
+
+  const handleAdd = (event: SyntheticEvent) => {
+    event.preventDefault();
+    if (counter === maxQuantity) return;
+    setCounter(counter + 1);
+  }
+
+  return (
+    <div className="grid grid-cols-[auto_1fr] gap-[1.25rem] mt-[2rem]">
+      <div className="flex gap-2 flex-nowrap items-center rounded-lg bg-border-color w-fit">
+        <button 
+            onClick={handleSubtract}
+            className="flex justify-center items-center h-8 w-8 text-lg font-bold text-main-contrast bg-transparent border-none p-0 cursor-pointer"
+        >
+        -</button>
+        <p className="m-0 text-[1.2rem] font-normal">{counter}</p>
+        <button 
+            onClick={handleAdd}
+            className="flex justify-center items-center h-8 w-8 text-lg font-bold text-main-contrast bg-transparent border-none p-0 cursor-pointer"
+        >+</button>
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="w-full"
+      >
+        <button
+          className="w-full h-12 flex flex-nowrap gap-2 items-center justify-center mt-4 p-2.5 border-none rounded-lg bg-gradient-to-r from-[rgb(193,98,253)] to-[rgb(0,49,165)] font-bold text-lg text-white"
+          type="submit"
+        >
+          <FaCartShopping />
+          <span>Add to cart</span>
+        </button>
+      </form>
+    </div>
+  )
+};
