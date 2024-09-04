c2fa876f4818d4898341b5d67b8cef8693e5e389
luischs23 <luisc.herreras@gmail.com>
2024-09-04 10:48:55 -0500

    Sanitize y SEO

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,13 @@
 import { Suspense } from "react";
 import { MainProduct } from "../../components/home/MainProducts";
 import { Loader } from "../../components/shared/Loader";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Future World",
+  description: "Welcome to the future world, an ecommerce from other century",
+  keywords: ["future", "world", "ecommerce", "technology"],
+}
 
 export default function Home() {
   return (
--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,19 +1,12 @@
-'use client'
-
 import Image from "next/image";
 import { ProductViewItemsOrder } from "./ProductViewItemsOrder";
-import { useRouter } from "next/navigation";
+import { SanitizeHTML } from "app/components/shared/SanitizeHTML/SanitizeHTML";
 
 interface ProductViewProps {
   product: ProductType
 }
 
 export const ProductView = ({ product }: ProductViewProps) => {
-  const router = useRouter()
-  if(!product){
-    router.push("/")
-  }
-
   return (
     <main className="max-w-[1200px] mx-auto grid grid-cols-[1fr_450px] gap-[5rem] mt-[5rem]">
       <section className="justify-self-end">
@@ -31,8 +24,9 @@ export const ProductView = ({ product }: ProductViewProps) => {
         <h1 className="text-2xl font-bold mt-0 mb-2">{product.title}</h1>
         <p className="w-fit text-lg leading-6 tracking-tighter border-2 border-main-contrast text-text-color text-glow py-2 px-4 rounded-xl mt-4 mb-0">
           {product.tags}</p>
-        <p className="text-[1.125rem] leading-[1.5] tracking-[0.5px] my-4">
-          {product.description}</p>
+        <SanitizeHTML tag="p" className="text-[1.125rem] leading-[1.5] tracking-[0.5px] my-4">
+          {product.description}
+        </SanitizeHTML>
         <span className="text-color-secondary text-[2.125rem] font-bold">
           $ {product.price}
         </span>
