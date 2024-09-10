b5133517d7ed3f4b43a78da7fec4ec1388d3f2d7
luischs23 <luisc.herreras@gmail.com>
2024-09-10 18:12:08 -0500

    Chat OpenAi

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,10 @@
+import { Chat } from "app/components/chat/chat"
+
+export default function ChatPage(){
+    return(
+        <>
+            <h1>ChatBot</h1>
+            <Chat />
+        </>
+    )
+}
