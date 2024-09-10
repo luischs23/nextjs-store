09cda7dd2598c2363b911b5c3ecebbd67d890ffb
luischs23 <luisc.herreras@gmail.com>
2024-09-10 18:29:18 -0500

    Chat Agent

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,30 +1,26 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai';
-
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
-
-// convert messages from the Vercel AI SDK Format to the format
-// that is expected by the Google GenAI SDK
-const buildGoogleGenAIPrompt = (messages: Message[]) => ({
-  contents: messages
-    .filter(message => message.role === 'user' || message.role === 'assistant')
-    .map(message => ({
-      role: message.role === 'user' ? 'user' : 'model',
-      parts: [{ text: message.content }],
-    })),
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+ 
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY,
 });
-
+ 
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+ 
 export async function POST(req: Request) {
-  // Extract the `prompt` from the body of the request
+  // Extract the `messages` from the body of the request
   const { messages } = await req.json();
-
-  const geminiStream = await genAI
-    .getGenerativeModel({ model: 'gemini-pro' })
-    .generateContentStream(buildGoogleGenAIPrompt(messages));
-
+ 
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages,
+  });
   // Convert the response into a friendly text-stream
-  const stream = GoogleGenerativeAIStream(geminiStream);
-
+  const stream = OpenAIStream(response);
   // Respond with the stream
   return new StreamingTextResponse(stream);
 }
