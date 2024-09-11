250e275c211eddd9e3a471bdaf3b8e93455503ad
luischs23 <luisc.herreras@gmail.com>
2024-09-10 19:49:57 -0500

    Bundle Analyze

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,25 @@
-import { hostname } from 'os';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const bundleAnalyzer = withBundleAnalyzer({
+	enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
-    images:{
-        remotePatterns:[{
-            hostname:'cdn.shopify.com',
-            protocol:'https',
-        }]
-    }
+  images:{
+    remotePatterns:[
+      {
+        hostname:'cdn.shopify.com',
+        protocol:'https'
+      }
+    ]
+  }
 };
 
-export default nextConfig;
+export default bundleAnalyzer(nextConfig);
